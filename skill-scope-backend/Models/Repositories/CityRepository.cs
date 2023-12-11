75bbd8f763033997042a38384b32c1369b08c0bf
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-11 04:15:05 -0700

    Created Country interface and repository

--- skill-scope-backend/Models/Repositories/CityRepository.cs
+++ skill-scope-backend/Models/Repositories/CityRepository.cs
@@ -5,58 +5,43 @@ using skill_scope_backend.Models;
 
 namespace skill_scope_backend.Repositories
 {
-    public class CityRepository : ICityRepository
+    public class CityRepository(string connectionString) : ICityRepository
     {
-        private readonly string _connectionString;
-
-        public CityRepository(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
+        private readonly string _connectionString = connectionString;
 
         public async Task<City?> GetByIdAsync(int cityId)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                return await db.QueryFirstOrDefaultAsync<City>(
-                    "SELECT * FROM cities WHERE city_id = @CityId", 
-                    new { CityId = cityId });
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            return await db.QueryFirstOrDefaultAsync<City>(
+                "SELECT * FROM cities WHERE city_id = @CityId",
+                new { CityId = cityId });
         }
 
         public async Task<IEnumerable<City>> GetAllAsync()
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                return await db.QueryAsync<City>("SELECT * FROM cities");
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            return await db.QueryAsync<City>("SELECT * FROM cities");
         }
 
         public async Task AddAsync(City city)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                var sql = "INSERT INTO cities (city_name, state_id) VALUES (@CityName, @StateId)";
-                await db.ExecuteAsync(sql, city);
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            var sql = "INSERT INTO cities (city_name, state_id) VALUES (@CityName, @StateId)";
+            await db.ExecuteAsync(sql, city);
         }
 
         public async Task UpdateAsync(City city)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                var sql = "UPDATE cities SET city_name = @CityName, state_id = @StateId WHERE city_id = @CityId";
-                await db.ExecuteAsync(sql, city);
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            var sql = "UPDATE cities SET city_name = @CityName, state_id = @StateId WHERE city_id = @CityId";
+            await db.ExecuteAsync(sql, city);
         }
 
         public async Task DeleteAsync(int cityId)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                await db.ExecuteAsync("DELETE FROM cities WHERE city_id = @CityId", 
-                                      new { CityId = cityId });
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            await db.ExecuteAsync("DELETE FROM cities WHERE city_id = @CityId",
+                                  new { CityId = cityId });
         }
     }
 }
--- skill-scope-backend/Models/Repositories/CompanyRepository.cs
+++ skill-scope-backend/Models/Repositories/CompanyRepository.cs
@@ -5,58 +5,43 @@ using skill_scope_backend.Models;
 
 namespace skill_scope_backend.Repositories
 {
-    public class CompanyRepository : ICompanyRepository
+    public class CompanyRepository(string connectionString) : ICompanyRepository
     {
-        private readonly string _connectionString;
-
-        public CompanyRepository(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
+        private readonly string _connectionString = connectionString;
 
         public async Task<Company?> GetByIdAsync(int companyId)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                return await db.QueryFirstOrDefaultAsync<Company>(
-                    "SELECT * FROM companies WHERE company_id = @CompanyId", 
-                    new { CompanyId = companyId });
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            return await db.QueryFirstOrDefaultAsync<Company>(
+                "SELECT * FROM companies WHERE company_id = @CompanyId",
+                new { CompanyId = companyId });
         }
 
         public async Task<IEnumerable<Company>> GetAllAsync()
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                return await db.QueryAsync<Company>("SELECT * FROM companies");
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            return await db.QueryAsync<Company>("SELECT * FROM companies");
         }
 
         public async Task<int> AddAsync(Company company)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                var sql = "INSERT INTO companies (company_name) VALUES (@CompanyName) RETURNING company_id";
-                return await db.QuerySingleAsync<int>(sql, company);
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            var sql = "INSERT INTO companies (company_name) VALUES (@CompanyName) RETURNING company_id";
+            return await db.QuerySingleAsync<int>(sql, company);
         }
 
         public async Task UpdateAsync(Company company)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                var sql = "UPDATE companies SET company_name = @CompanyName WHERE company_id = @CompanyId";
-                await db.ExecuteAsync(sql, company);
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            var sql = "UPDATE companies SET company_name = @CompanyName WHERE company_id = @CompanyId";
+            await db.ExecuteAsync(sql, company);
         }
 
         public async Task DeleteAsync(int companyId)
         {
-            using (IDbConnection db = new NpgsqlConnection(_connectionString))
-            {
-                await db.ExecuteAsync("DELETE FROM companies WHERE company_id = @CompanyId", 
-                                      new { CompanyId = companyId });
-            }
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            await db.ExecuteAsync("DELETE FROM companies WHERE company_id = @CompanyId",
+                                  new { CompanyId = companyId });
         }
     }
 }
