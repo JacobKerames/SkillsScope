833da32fbec87795bc9af854dc2b9c25a04d386e
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-11 16:07:04 -0700

    Connected backend desired skills for title functionality to frontend

--- skill-scope/src/app/components/SearchForm.tsx
+++ skill-scope/src/app/components/SearchForm.tsx
@@ -7,27 +7,19 @@ const SearchForm = () => {
 
   const handleSearch = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (!searchTerm.trim()) {
+      console.error("Search term is empty");
+      return;
+    }
     try {
-      const response = await fetch("http://localhost:5277/search/perform", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ query: searchTerm }),
-      });
-
+      const response = await fetch(`http://localhost:5277/search/perform/${encodeURIComponent(searchTerm)}`);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-
-      // Process the response data as required
       const data = await response.json();
       console.log(data);
     } catch (error) {
-      console.error(
-        "There has been a problem with your fetch operation:",
-        error
-      );
+      console.error("There has been a problem with your fetch operation:", error);
     }
   };
 
@@ -39,6 +31,8 @@ const SearchForm = () => {
           type="text"
           placeholder="Search job titles..."
           aria-label="Job title search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button
           className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
