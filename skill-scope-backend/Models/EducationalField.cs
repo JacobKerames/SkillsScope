8a6070c1e112436b26aa3fdb236e9d9d12cdcec5
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-11 04:17:39 -0700

    Created EducationalField interface and repository

--- skill-scope-backend/Models/EducationalField.cs
+++ skill-scope-backend/Models/EducationalField.cs
@@ -2,5 +2,5 @@ namespace skill_scope_backend.Models;
 public class EducationalField
 {
     public int EducationalFieldId { get; set; }
-    public string EducationalFieldName { get; set; }
+    public required string EducationalFieldName { get; set; }
 }
---
+++ skill-scope-backend/Models/Repositories/IEducationalField.cs
@@ -0,0 +1,13 @@
+using skill_scope_backend.Models;
+
+namespace skill_scope_backend.Repositories
+{
+    public interface IEducationalFieldRepository
+    {
+        Task<EducationalField?> GetByIdAsync(int educationalFieldId);
+        Task<IEnumerable<EducationalField>> GetAllAsync();
+        Task<int> AddAsync(EducationalField educationalField);
+        Task UpdateAsync(EducationalField educationalField);
+        Task DeleteAsync(int educationalFieldId);
+    }
+}
