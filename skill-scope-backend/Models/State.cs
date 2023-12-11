d0e85c4781e3f1805ab21d1035a57c597fd5417a
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-11 04:38:43 -0700

    Created State interface and repository

--- skill-scope-backend/Models/State.cs
+++ skill-scope-backend/Models/State.cs
@@ -2,7 +2,7 @@ namespace skill_scope_backend.Models;
 public class State
 {
     public int StateId { get; set; }
-    public string StateName { get; set; }
+    public required string StateName { get; set; }
     public string? StateAbbr { get; set; }
     public int CountryId { get; set; }
 }
