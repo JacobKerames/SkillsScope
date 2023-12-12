2b7a48a52dd159ce2c300dfc9e8e0f61520bc198
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-11 21:06:36 -0700

    Created method to return the commonality of various educational levels and fields for different job titles

--- skill-scope-backend/Repositories/JobPostingRepository.cs
+++ skill-scope-backend/Repositories/JobPostingRepository.cs
@@ -21,19 +21,38 @@ namespace skill_scope_backend.Repositories
                 GROUP BY s.SkillId, s.SkillName";
 
             using IDbConnection db = new NpgsqlConnection(_connectionString);
-            var skillPercentages = await db.QueryAsync<dynamic>(sql, new { Keyword = keyword });
+            var skillStats = await db.QueryAsync<SkillDTO>(sql, new { Keyword = keyword });
 
-            return skillPercentages.Select(sp => new SkillDTO
-            {
-                Skill = new Skill { SkillId = sp.SkillId, SkillName = sp.SkillName },
-                Percentage = sp.Percentage
-            });
+            return skillStats;
+        }
+
+        public async Task<IEnumerable<EducationDTO>> GetTitleEducationDesireAsync(string keyword)
+        {
+            var sql = @"
+                SELECT eq.EducationLevel, ef.EducationalFieldName,
+                    COUNT(*) * 100.0 / SUM(COUNT(*)) OVER() AS Percentage
+                FROM JobPostings jp
+                JOIN EducationalQualifications eq ON jp.JobPostingId = eq.JobPostingId
+                JOIN EducationalFields ef ON eq.EducationalFieldId = ef.EducationalFieldId
+                WHERE CONTAINS(jp.Title, @Keyword)
+                GROUP BY eq.EducationLevel, ef.EducationalFieldName";
+
+            using IDbConnection db = new NpgsqlConnection(_connectionString);
+            var educationStats = await db.QueryAsync<EducationDTO>(sql, new { Keyword = keyword });
+
+            return educationStats;
         }
 
         public async Task<int> AddAsync(JobPosting jobPosting)
         {
             using IDbConnection db = new NpgsqlConnection(_connectionString);
-            var sql = "INSERT INTO job_postings (title, company_id, city_id, state_id, country_id, posted_date) VALUES (@Title, @CompanyId, @CityId, @StateId, @CountryId, @PostedDate) RETURNING job_posting_id";
+            var sql = @"
+                INSERT INTO job_postings 
+                (title, company_id, city_id, state_id, country_id, posted_date) 
+                VALUES 
+                (@Title, @CompanyId, @CityId, @StateId, @CountryId, @PostedDate) 
+                RETURNING job_posting_id";
+                
             return await db.QuerySingleAsync<int>(sql, jobPosting);
         }
     }
