d05412c708c6726903f1666f90b3dc5b4f81c3af
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-20 08:46:47 -0700

    Converted layout to use mantine

--- skill-scope/src/app/layout.tsx
+++ skill-scope/src/app/layout.tsx
@@ -1,13 +1,21 @@
-import './globals.css';
+import "./globals.css";
+import "@mantine/core/styles.css";
+
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
-  )
+  );
 }
