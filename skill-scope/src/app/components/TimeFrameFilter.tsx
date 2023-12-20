ead785e5739f59009c1c47dc413bea5114772331
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-20 09:11:41 -0700

    Converted time frame filter component to use mantine

--- skill-scope/src/app/components/TimeFrameFilter.tsx
+++ skill-scope/src/app/components/TimeFrameFilter.tsx
@@ -1,79 +1,32 @@
-import { MenuProps } from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-
-const menuProps: Partial<MenuProps> = {
-  PaperProps: {
-    sx: {
-			color: '#E6E6E6',
-      bgcolor: '#101010',
-			border: '1px solid gray',
-      '& .MuiMenuItem-root': {
-        '&:hover': {
-          backgroundColor: '#212121',
-        },
-      },
-    },
-  },
-};
+import { Select } from '@mantine/core';
 
 interface TimeFrameFilterProps {
 	timeFrame: string;
-  onTimeFrameChange: (value: string) => void;
+  onTimeFrameChange: (value: string | null) => void;
 }
 
 const TimeFrameFilter: React.FC<TimeFrameFilterProps> = ({ timeFrame, onTimeFrameChange }) => {
-  const handleChange = (event: SelectChangeEvent) => {
-    onTimeFrameChange(event.target.value);
+	const timeFrameOptions = [
+    { value: '', label: 'Any' },
+    { value: 'pastMonth', label: 'Past Month' },
+    { value: 'pastYear', label: 'Past Year' },
+    { value: 'pastTwoYears', label: 'Past Two Years' },
+    { value: 'pastFiveYears', label: 'Past Five Years' },
+  ];
+
+  const handleChange = (value: string | null) => {
+    onTimeFrameChange(value); // Now handles null as well
   };
 
   return (
     <div className="flex items-center border-b border-teal-500 py-2">
-      <FormControl
-				variant="standard"
-				sx={{ 
-					width: '100%'
-				}}
-			>
-        <Select
-          id="time-frame-select"
-          value={timeFrame}
-          onChange={handleChange}
-					disableUnderline
-					displayEmpty
-					renderValue={(selected) => {
-						if (selected === '') {
-							return <span style={{ color: '#9CA3AF', fontStyle: 'normal' }}>Time Frame</span>;
-						}
-						switch (selected) {
-							case 'pastMonth':
-								return 'Past Month';
-							case 'pastYear':
-								return 'Past Year';
-							case 'pastTwoYears':
-								return 'Past Two Years';
-							case 'pastFiveYears':
-								return 'Past Five Years';
-							default:
-								return selected;
-						}
-					}}
-					MenuProps={menuProps}
-					sx={{
-						color: '#E6E6E6',
-						'& .MuiSelect-select': {
-							padding: '4px 8px',
-						}
-					}}
-        >
-          <MenuItem value="">Any</MenuItem>
-          <MenuItem value='pastMonth'>Past Month</MenuItem>
-          <MenuItem value='pastYear'>Past Year</MenuItem>
-          <MenuItem value='pastTwoYears'>Past Two Years</MenuItem>
-					<MenuItem value='pastFiveYears'>Past Five Years</MenuItem>
-        </Select>
-      </FormControl>
+      <Select
+        id="time-frame-select"
+        value={timeFrame}
+        onChange={handleChange}
+        placeholder="Time Frame"
+        data={timeFrameOptions}
+      />
     </div>
   );
 };
