29b856aade26b2166f5ba4430f17df7a3cac720a
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-19 20:35:02 -0700

    Updated DTOs

--- skill-scope-backend/Models/ExperienceDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace skill_scope_backend.Models;
-public class ExperienceDTO
-{
-    public required string ExperienceReference { get; set; }
-    public int YearsExperience { get; set; }
-    public decimal Percentage { get; set; }
-}
