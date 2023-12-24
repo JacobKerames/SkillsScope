f5e0ed29a190f5eec8ef5bbeceffb516d65b0e19
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-24 09:01:30 -0700

    Defined scrapy items

--- skills-scope-scraper/job_listing_scraper/job_listing_scraper/items.py
+++ skills-scope-scraper/job_listing_scraper/job_listing_scraper/items.py
@@ -5,8 +5,33 @@
 
 import scrapy
 
+class JobListingItem(scrapy.Item):
+    job_listing_id = scrapy.Field()
+    title = scrapy.Field()
+    company_name = scrapy.Field()
+    city_name = scrapy.Field()
+    state_name = scrapy.Field()
+    country_name = scrapy.Field()
+    posted_date = scrapy.Field()
+    salary_lower_bound = scrapy.Field()
+    salary_upper_bound = scrapy.Field()
+    experience_level = scrapy.Field()
 
-class JobListingScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CompanyItem(scrapy.Item):
+    company_name = scrapy.Field()
+    logo_url = scrapy.Field()
+    industry = scrapy.Field()
+    company_size_lower_bound = scrapy.Field()
+    company_size_upper_bound = scrapy.Field()
+    last_updated = scrapy.Field()
+
+class EducationalQualificationItem(scrapy.Item):
+    job_listing_id = scrapy.Field()
+    education_level = scrapy.Field()
+    educational_field_name = scrapy.Field()
+
+class ExperienceQualificationItem(scrapy.Item):
+    job_listing_id = scrapy.Field()
+    years_experience = scrapy.Field()
+    qualification = scrapy.Field()
+    experience_type = scrapy.Field()
